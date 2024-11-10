6841b68a731afcf55dc6d813965c9811d40fa518
RajeshMahendran <rajeshmahendran1999@gmail.com>
2024-11-09 21:13:15 -0600

    Add navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -7,21 +7,21 @@ const NavBar = () => {
   const [lightingDropdownVisible, setLightingDropdownVisible] = useState(false);
   const [signsDropdownVisible, setSignsDropdownVisible] = useState(false);
 
-  // Toggle lighting dropdown
-  const handleLightingMouseEnter = () => {
-    setLightingDropdownVisible(true);
+  // Toggle lighting dropdown on click
+  const handleLightingClick = () => {
+    setLightingDropdownVisible((prev) => !prev);
+    setSignsDropdownVisible(false); // Ensure only one dropdown is open at a time
   };
 
-  const handleLightingMouseLeave = () => {
+  // Toggle signs dropdown on click
+  const handleSignsClick = () => {
+    setSignsDropdownVisible((prev) => !prev);
+    setLightingDropdownVisible(false); // Ensure only one dropdown is open at a time
+  };
+
+  // Hide dropdown when a dropdown link is clicked
+  const handleDropdownItemClick = () => {
     setLightingDropdownVisible(false);
-  };
-
-  // Toggle signs dropdown on hover
-  const handleSignsMouseEnter = () => {
-    setSignsDropdownVisible(true);
-  };
-
-  const handleSignsMouseLeave = () => {
     setSignsDropdownVisible(false);
   };
 
@@ -31,12 +31,8 @@ const NavBar = () => {
         <img src={logo} alt="Logo" className="logo-image" />
       </NavLink>
       <ul className="navbar-links">
-        <li
-          className="dropdown-container"
-          onMouseEnter={handleLightingMouseEnter}
-          onMouseLeave={handleLightingMouseLeave}
-        >
-          <a href="#" className="nav-link">
+        <li className="dropdown-container">
+          <a href="#" className="nav-link" onClick={handleLightingClick}>
             LIGHTING
           </a>
           {lightingDropdownVisible && (
@@ -44,46 +40,42 @@ const NavBar = () => {
               <div className="dropdown-section">
                 <h3>INDOOR</h3>
                 <ul>
-                  <li><NavLink to="/lighting/cooler-light">COOLER LIGHT</NavLink></li>
-                  <li><NavLink to="/lighting/corn-light">CORN LIGHT</NavLink></li>
-                  <li><NavLink to="/lighting/downlight-series">DOWNLIGHT SERIES</NavLink></li>
-                  <li><NavLink to="/lighting/gimbal-light">GIMBAL LIGHT</NavLink></li>
-                  <li><NavLink to="/lighting/integrated-tube-light">INTEGRATED TUBE LIGHT</NavLink></li>
-                  <li><NavLink to="/lighting/panels">PANELS</NavLink></li>
-                  <li><NavLink to="/lighting/t8-tube-light">T8 TUBE LIGHT</NavLink></li>
-                  <li><NavLink to="/lighting/troffers">TROFFERS</NavLink></li>
-                  <li><NavLink to="/lighting/ufo-highbay">UFO HIGHBAY</NavLink></li>
+                  <li><NavLink to="/lighting/cooler-light" onClick={handleDropdownItemClick}>COOLER LIGHT</NavLink></li>
+                  <li><NavLink to="/lighting/corn-light" onClick={handleDropdownItemClick}>CORN LIGHT</NavLink></li>
+                  <li><NavLink to="/lighting/downlight-series" onClick={handleDropdownItemClick}>DOWNLIGHT SERIES</NavLink></li>
+                  <li><NavLink to="/lighting/gimbal-light" onClick={handleDropdownItemClick}>GIMBAL LIGHT</NavLink></li>
+                  <li><NavLink to="/lighting/integrated-tube-light" onClick={handleDropdownItemClick}>INTEGRATED TUBE LIGHT</NavLink></li>
+                  <li><NavLink to="/lighting/panels" onClick={handleDropdownItemClick}>PANELS</NavLink></li>
+                  <li><NavLink to="/lighting/t8-tube-light" onClick={handleDropdownItemClick}>T8 TUBE LIGHT</NavLink></li>
+                  <li><NavLink to="/lighting/troffers" onClick={handleDropdownItemClick}>TROFFERS</NavLink></li>
+                  <li><NavLink to="/lighting/ufo-highbay" onClick={handleDropdownItemClick}>UFO HIGHBAY</NavLink></li>
                 </ul>
               </div>
               <div className="dropdown-section">
                 <h3>OUTDOOR</h3>
                 <ul>
-                  <li><NavLink to="/lighting/canopy-light">CANOPY LIGHT</NavLink></li>
-                  <li><NavLink to="/lighting/flood-light">FLOOD LIGHT</NavLink></li>
-                  <li><NavLink to="/lighting/shoe-box">SHOE BOX</NavLink></li>
-                  <li><NavLink to="/lighting/exit-signs">EXIT SIGNS</NavLink></li>
-                  <li><NavLink to="/lighting/wall-packs">WALL PACKS</NavLink></li>
+                  <li><NavLink to="/lighting/canopy-light" onClick={handleDropdownItemClick}>CANOPY LIGHT</NavLink></li>
+                  <li><NavLink to="/lighting/flood-light" onClick={handleDropdownItemClick}>FLOOD LIGHT</NavLink></li>
+                  <li><NavLink to="/lighting/shoe-box" onClick={handleDropdownItemClick}>SHOE BOX</NavLink></li>
+                  <li><NavLink to="/lighting/exit-signs" onClick={handleDropdownItemClick}>EXIT SIGNS</NavLink></li>
+                  <li><NavLink to="/lighting/wall-packs" onClick={handleDropdownItemClick}>WALL PACKS</NavLink></li>
                 </ul>
               </div>
             </div>
           )}
         </li>
 
-        {/* Signs dropdown (Updated for hover functionality) */}
-        <li
-          className="dropdown-container"
-          onMouseEnter={handleSignsMouseEnter}
-          onMouseLeave={handleSignsMouseLeave}
-        >
-          <a href="#" className="nav-link">
+        {/* Signs dropdown */}
+        <li className="dropdown-container">
+          <a href="#" className="nav-link" onClick={handleSignsClick}>
             SIGNS
           </a>
           {signsDropdownVisible && (
             <div className="dropdown">
               <div className="dropdown-section">
                 <ul>
-                  <li><NavLink to="/signs/gas-price-sign">GAS PRICE SIGN</NavLink></li>
-                  <li><NavLink to="/signs/display-sign">DISPLAY SIGN</NavLink></li>
+                  <li><NavLink to="/signs/gas-price-sign" onClick={handleDropdownItemClick}>GAS PRICE SIGN</NavLink></li>
+                  <li><NavLink to="/signs/display-sign" onClick={handleDropdownItemClick}>DISPLAY SIGN</NavLink></li>
                 </ul>
               </div>
             </div>
@@ -91,12 +83,12 @@ const NavBar = () => {
         </li>
 
         <li>
-          <NavLink to="/cctv" className="nav-link">
+          <NavLink to="/cctv" className="nav-link" onClick={handleDropdownItemClick}>
             CCTV
           </NavLink>
         </li>
         <li>
-          <NavLink to="/contactus" className="nav-link">
+          <NavLink to="/contactus" className="nav-link" onClick={handleDropdownItemClick}>
             CONTACT US
           </NavLink>
         </li>
